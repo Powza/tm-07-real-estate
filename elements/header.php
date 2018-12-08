5aeccddfd884eb8b6e5acd224907fe1a447997f3
Derek <derek@tinyminute.com>
2018-12-08 10:38:30 -0500

    import

--- elements/header.php
+++ elements/header.php
@@ -40,6 +40,100 @@
 </script>
 
 <header class="header">
+	<nav class="navbar admin-navbar navbar-default navbar-inverse hidden-xs hidden-sm">
+	    <div class="container-fluid">
+	        <div class="navbar-header">
+	            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	                <span class="sr-only">Toggle navigation</span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	            </button>
+	            <a class="navbar-brand hidden-sm hidden-md hidden-lg" href="/admin">Admin</a>
+	        </div>
+
+	        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	            <ul class="nav navbar-nav admin-nav">
+	                <li><a href="/admin/home"><i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard</a></li>
+	                <li class="dropdown">
+	                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-files-o" aria-hidden="true"></i> Manage</a>
+	                    <ul class="dropdown-menu">
+	                        <li><a href="/admin/pages">Pages</a></li>
+	                        <li><a href="/admin/forms">Forms</a></li>
+	                        <li><a href="/admin/blogs">Posts</a></li>
+	                        <li><a href="/admin/menu_lookups">Menus</a></li>
+	                        <li><a href="/admin/file_manager">Files</a></li>
+	                    </ul>
+	                </li>
+	                <li class="dropdown">
+	                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-plus" aria-hidden="true"></i> New</a>
+	                    <ul class="dropdown-menu">
+	                        <li><a href="/admin/pages/add">Page</a></li>
+	                        <li><a href="/admin/forms/add">Form</a></li>
+	                        <li><a href="/admin/blogs/add">Post</a></li>
+	                        <li><a href="/admin/menu_lookups/add">Menu</a></li>
+	                    </ul>
+	                </li>
+
+	                                <li><a href="/admin/pages/edit/4db037d8-88d4-4794-a96b-27f896e92e70"><i class="fa fa-pencil" aria-hidden="true"></i> Edit Page</a></li>
+	                
+	                                
+	                
+	                
+	                
+	                
+	                
+	                
+	                
+	                
+	                <li class="dropdown">
+	                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-area-chart" aria-hidden="true"></i> MLS</a>
+	                    <ul class="dropdown-menu">
+	                        <li class="dropdown-header">Last Updated</li>
+	                        <li class="dropdown-body">
+	                            11-28-2018 8:33am                        </li>
+	                    </ul>
+	                </li>
+
+	                
+	                <li class="dropdown">
+	                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-cogs" aria-hidden="true"></i> Sitewide</a>
+	                    <ul class="dropdown-menu">
+	                        <li><a href="/admin/custom_views">Layouts/Elements</a></li>
+	                        <li><a href="/admin/settings">Settings</a></li>
+	                    </ul>
+	                </li>
+	            </ul>
+	            <ul class="nav navbar-nav navbar-right">
+	                <li class="dropdown">
+	                    </li><li><a href="/logout.html"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
+	                
+	            </ul>
+	        </div><!-- /.navbar-collapse -->
+	    </div><!-- /.container-fluid -->
+	</nav>
+
+	<style>
+	.admin-navbar { border-radius: 0; margin: 0; min-height: 30px; border: 0; background-color: #333; }
+	.admin-navbar .navbar-nav a { transition: 0.2s; }
+	.admin-navbar .navbar-nav > li > a { padding-top: 5px; padding-bottom: 5px; font-size: 13px; font-weight: 700; color: #eee; }
+	.admin-navbar .navbar-nav > li > a:hover { color: #0baf4d; }
+	.admin-navbar.navbar-inverse .navbar-nav > .open > a, .admin-navbar.navbar-inverse .navbar-nav > .open > a:focus, .admin-navbar.navbar-inverse .navbar-nav > .open > a:hover { background: #333; color: #0baf4d; }
+	.admin-navbar .dropdown.open .dropdown-menu { background: #333; border-color: #333; border-radius: 0; }
+	.admin-navbar .dropdown.open .dropdown-menu > li > a { color: #eee; background: none; }
+	.admin-navbar .dropdown.open .dropdown-menu > li > a:hover { background: none; color: #0baf4d; }
+	@media (min-width: 768px) { .admin-nav { margin-left: -15px; } }
+	.dropdown-header { color: #0baf4d; }
+	.dropdown-body { display: block; padding: 3px 20px; line-height: 1.42857143; color: #fff; white-space: nowrap; }
+	.dropdown-submenu { position: relative; }
+	.dropdown-submenu > .dropdown-menu { top: 0; left: 100%; margin-top: -6px; margin-left: -1px; border-radius: 0 6px 6px 6px; }
+	.dropdown-submenu:hover > .dropdown-menu { display: block; }
+	.dropdown-submenu > a:after { display: block; content: " "; float: right; width: 0; height: 0; border-color: transparent; border-style: solid; border-width: 5px 0 5px 5px; border-left-color: #ccc; margin-top: 5px; margin-right: -10px; }
+	.dropdown-submenu:hover > a:after { border-left-color: #fff; }
+	.dropdown-submenu.pull-left { float: none; }
+	.dropdown-submenu.pull-left > .dropdown-menu { left: -100%; margin-left: 10px; border-radius: 6px 0 6px 6px; }
+	</style>
+
 	<div class="header__meta hidden-xs hidden-sm">
 		<div class="header__container container-fluid container-fluid-edge-space">
 			<div class="header__nav hidden-xs hidden-sm">
@@ -172,7 +266,7 @@
 		</div>
 	</div>
 </header>
-<div class="start-scroll"></div>
+<div class="start-scroll1"></div>
 <div class="mobile-menu hidden-md hidden-lg">
 	<div class="mobile-menu__menu-container">
 		<div class="mobile-menu__account">
--- js/min/tm-07.scripts.min-min.js
+++ js/min/tm-07.scripts.min-min.js
@@ -2,29 +2,71 @@ $(function(){
 // Use listing image as background image
 function e(){if($(".listings_view .royalSlider").length){var e=$(".listings_view .royalSlider .rsNavSelected img").attr("src");$(".banner.section__background").css("background-image","url("+e+")")}}
 // Sidebar Scoll List Box
-function t(){$(".sidebar .sidebar__list-scroll").each(function(e){$(this).attr("id","list"+e),$(".sidebar .sidebar__list-scroll#list"+e+" ul").html($(".sidebar .sidebar__list-scroll#list"+e+" ul li").sort(function(){return Math.random()-.5}))}),$(".sidebar .sidebar__list-scroll .list-slider ul").each(function(e,t){var i="carousel"+e;this.id=i,$(this).slick({vertical:!0,verticalSwiping:!0,slidesToShow:6,slidesToScroll:6,prevArrow:'<button type="button" class="slick-prev btn btn__light__outline"><i class="fa fa-angle-up"></i></button>',nextArrow:'<button type="button" class="slick-next btn btn__light__outline"><i class="fa fa-angle-down"></i></button>',appendArrows:$("<div></div>").addClass("section__slider-arrows").appendTo(".sidebar__list-scroll#list"+e+" h4")})})}
+function i(){$(".sidebar .sidebar__list-scroll").each(function(e){$(this).attr("id","list"+e),$(".sidebar .sidebar__list-scroll#list"+e+" ul").html($(".sidebar .sidebar__list-scroll#list"+e+" ul li").sort(function(){return Math.random()-.5}))}),$(".sidebar .sidebar__list-scroll .list-slider ul").each(function(e,i){var t="carousel"+e;this.id=t,$(this).slick({vertical:!0,verticalSwiping:!0,slidesToShow:6,slidesToScroll:6,prevArrow:'<button type="button" class="slick-prev btn btn__light__outline"><i class="fa fa-angle-up"></i></button>',nextArrow:'<button type="button" class="slick-next btn btn__light__outline"><i class="fa fa-angle-down"></i></button>',appendArrows:$("<div></div>").addClass("section__slider-arrows").appendTo(".sidebar__list-scroll#list"+e+" h4")})})}
 // Homepage Property Slider
-function i(){$(".property-slider:not(.sidebar .property-slider)").each(function(e){$(this).attr("id","slide_"+e)}),$(".property-slider:not(.sidebar .property-slider) .slider-wrap").each(function(e,t){var i="carousel_"+e;this.id=i,$(this).slick({slidesToShow:4,slidesToScroll:4,rows:2,prevArrow:'<button type="button" class="slick-prev btn btn__primary__outline"><i class="fa fa-angle-left"></i> Prev</button>',nextArrow:'<button type="button" class="slick-next btn btn__primary__outline">Next <i class="fa fa-angle-right"></i></button>',appendArrows:$("<div></div>").addClass("section__slider-arrows").appendTo($(this).parent()),responsive:[{breakpoint:1680,settings:{slidesToShow:3,slidesToScroll:3}},{breakpoint:992,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:767,settings:{slidesToShow:1,slidesToScroll:1}}]})})}
+function t(){$(".property-slider:not(.sidebar .property-slider)").each(function(e){$(this).attr("id","slide_"+e)}),$(".property-slider:not(.sidebar .property-slider) .slider-wrap").each(function(e,i){var t="carousel_"+e;this.id=t,$(this).slick({slidesToShow:4,slidesToScroll:4,rows:2,prevArrow:'<button type="button" class="slick-prev btn btn__primary__outline"><i class="fa fa-angle-left"></i> Prev</button>',nextArrow:'<button type="button" class="slick-next btn btn__primary__outline">Next <i class="fa fa-angle-right"></i></button>',appendArrows:$("<div></div>").addClass("section__slider-arrows").appendTo($(this).parent()),responsive:[{breakpoint:1680,settings:{slidesToShow:3,slidesToScroll:3}},{breakpoint:992,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:767,settings:{slidesToShow:1,slidesToScroll:1}}]})})}
 // Sidebar Property Slider
-function s(){$(".sidebar .property-slider").each(function(e){$(this).attr("id","sidebar_slide_"+e),$(".sidebar .property-slider#sidebar_slide_"+e+" .slider-wrap").html($(".sidebar .property-slider .item").sort(function(){return Math.random()-.5}))}),$(".sidebar .property-slider .slider-wrap").each(function(e,t){var i="carousel_"+e;this.id=i,$(this).slick({adaptiveHeight:!0,prevArrow:'<button type="button" class="slick-prev btn btn__primary__outline"><i class="fa fa-angle-left"></i> Prev</button>',nextArrow:'<button type="button" class="slick-next btn btn__primary__outline">Next <i class="fa fa-angle-right"></i></button>',appendArrows:$("<div></div>").addClass("section__slider-arrows").appendTo($(this).parent())})})}
+function r(){$(".sidebar .property-slider").each(function(e){$(this).attr("id","sidebar_slide_"+e),$(".sidebar .property-slider#sidebar_slide_"+e+" .slider-wrap").html($(".sidebar .property-slider .item").sort(function(){return Math.random()-.5}))}),$(".sidebar .property-slider .slider-wrap").each(function(e,i){var t="carousel_"+e;this.id=t,$(this).slick({adaptiveHeight:!0,prevArrow:'<button type="button" class="slick-prev btn btn__primary__outline"><i class="fa fa-angle-left"></i> Prev</button>',nextArrow:'<button type="button" class="slick-next btn btn__primary__outline">Next <i class="fa fa-angle-right"></i></button>',appendArrows:$("<div></div>").addClass("section__slider-arrows").appendTo($(this).parent())})})}
 // Homepage Agent Slider
-function r(){$(".agent-slider .slider-wrap").html($(".agent-slider .slider-wrap .agent").sort(function(){return Math.random()-.5})),$(".agent-slider .agent-img").Lazy({scrollDirection:"vertical"}),$(".agent-slider .slider-wrap").slick({slidesToShow:5,slidesToScroll:5,prevArrow:'<button type="button" class="slick-prev btn btn__primary__outline"><i class="fa fa-angle-left"></i> Prev</button>',nextArrow:'<button type="button" class="slick-next btn btn__primary__outline">Next <i class="fa fa-angle-right"></i></button>',appendArrows:$("<div></div>").addClass("section__slider-arrows").appendTo(".agent-slider"),responsive:[{breakpoint:1680,settings:{slidesToShow:4,slidesToScroll:4}},{breakpoint:1440,settings:{slidesToShow:3,slidesToScroll:3}},{breakpoint:1100,settings:{slidesToShow:3,slidesToScroll:3}},{breakpoint:992,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:767,settings:{slidesToShow:1,slidesToScroll:1}}]})}
+function s(){$(".agent-slider .slider-wrap").html($(".agent-slider .slider-wrap .agent").sort(function(){return Math.random()-.5})),$(".agent-slider .agent-img").Lazy({scrollDirection:"vertical"}),$(".agent-slider .slider-wrap").slick({slidesToShow:5,slidesToScroll:5,prevArrow:'<button type="button" class="slick-prev btn btn__primary__outline"><i class="fa fa-angle-left"></i> Prev</button>',nextArrow:'<button type="button" class="slick-next btn btn__primary__outline">Next <i class="fa fa-angle-right"></i></button>',appendArrows:$("<div></div>").addClass("section__slider-arrows").appendTo(".agent-slider"),responsive:[{breakpoint:1680,settings:{slidesToShow:4,slidesToScroll:4}},{breakpoint:1440,settings:{slidesToShow:3,slidesToScroll:3}},{breakpoint:1100,settings:{slidesToShow:3,slidesToScroll:3}},{breakpoint:992,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:767,settings:{slidesToShow:1,slidesToScroll:1}}]})}
 // RoyalSlider Fullscreen Hide Header
 function o(){if($(".listings_view .royalSlider").length){var e=$(".royalSlider").data("royalSlider");e.ev.on("rsEnterFullscreen",function(){$(".header").hide()}),e.ev.on("rsExitFullscreen",function(){$(".header").show()})}}
+// Scroll Navigation Menu
+/*$(window).scroll(function () {
+        var scroll = $(this).scrollTop();
+        var topDist = $(".start-scroll").position();
+        if (scroll > $('header').outerHeight()) {
+            $('.header__meta').addClass('fixed');
+        } else {
+            $('.header__meta').removeClass('fixed');
+        }
+    });*/
 // Hide Header on scroll down
-function l(){function e(){var e=$(this).scrollTop();
-// Make sure they scroll more than delta
-Math.abs(i-e)<=s||(i<e&&r<e?
-// Scroll Down
-$(".header__meta").css({opacity:"0",transform:"translateY(-100%)"}):
-// Scroll Up
-e+$(window).height()<$(document).height()&&$(".header__meta").css({opacity:"1",transform:"translateY(0)"}),i=e)}var t,i=0,s=46,r=$("header").outerHeight();$(window).scroll(function(e){t=!0}),setInterval(function(){t&&(e(),t=!1)},0)}
+/*function headerMenuScroll() {
+        var didScroll;
+        var lastScrollTop = 0;
+        var delta = 46;
+        var navbarHeight = $('header').outerHeight();
+
+        $(window).scroll(function(event){
+            didScroll = true;
+        });
+
+        setInterval(function() {
+            if (didScroll) {
+                hasScrolled();
+                didScroll = false;
+            }
+        }, 0);
+
+        function hasScrolled() {
+            var st = $(this).scrollTop();
+            
+            // Make sure they scroll more than delta
+            if(Math.abs(lastScrollTop - st) <= delta)
+                return;
+            
+            if (st > lastScrollTop && st > navbarHeight){
+                // Scroll Down
+                $('.header__meta').css({'opacity':'0', 'transform':'translateY(-100%)'});
+            } else {
+                // Scroll Up
+                if(st + $(window).height() < $(document).height()) {
+                    $('.header__meta').css({'opacity':'1', 'transform':'translateY(0)'});
+                }
+            }
+            
+            lastScrollTop = st;
+        }
+    }
+    headerMenuScroll();*/
+function l(){var e=document.querySelector(".header .header__meta"),i;window.location.hash&&e.classList.add("headroom--unpinned"),new Headroom(e,{tolerance:{down:10,up:20},offset:$(".admin-navbar").outerHeight()+$(".header").outerHeight()}).init()}
 // Header padding
 function a(){$("body.int").css("padding-top",$(".header").height()+"px")}
 // Responsive Embed
-function n(){var e,t=[
+function n(){var e,i=[
 //'iframe[src*="player.vimeo.com"]',
-'iframe[src*="youtube.com"]','iframe[src*="youtube-nocookie.com"]','iframe[src*="kickstarter.com"][src*="video.html"]','iframe[src*="screenr.com"]','iframe[src*="blip.tv"]','iframe[src*="dailymotion.com"]','iframe[src*="viddler.com"]','iframe[src*="qik.com"]','iframe[src*="revision3.com"]','iframe[src*="hulu.com"]','iframe[src*="funnyordie.com"]','iframe[src*="flickr.com"]','embed[src*="v.wordpress.com"]'].join(",");$(t).wrap('<span class="fluid-video" />')}
+'iframe[src*="youtube.com"]','iframe[src*="youtube-nocookie.com"]','iframe[src*="kickstarter.com"][src*="video.html"]','iframe[src*="screenr.com"]','iframe[src*="blip.tv"]','iframe[src*="dailymotion.com"]','iframe[src*="viddler.com"]','iframe[src*="qik.com"]','iframe[src*="revision3.com"]','iframe[src*="hulu.com"]','iframe[src*="funnyordie.com"]','iframe[src*="flickr.com"]','embed[src*="v.wordpress.com"]'].join(",");$(i).wrap('<span class="fluid-video" />')}
 // Homepage Agent Testimonials
 function d(){$(".testimonials-slider .slider-wrap").html($(".testimonials-slider blockquote").sort(function(){return Math.random()-.5})),$(".testimonials-slider .slider-wrap").slick({adaptiveHeight:!0,prevArrow:'<button type="button" class="slick-prev btn btn__primary__outline"><i class="fa fa-angle-left"></i> Prev</button>',nextArrow:'<button type="button" class="slick-next btn btn__primary__outline">Next <i class="fa fa-angle-right"></i></button>',appendArrows:$("<div></div>").addClass("section__slider-arrows").appendTo(".testimonials-slider")})}
 // Hero Slider
@@ -34,7 +76,7 @@ function u(){var e=".hero__video";$(e).YTPlayer({videoURL:$(e).data("video-url")
 // Check if media type is defind and default if empty
 function p(){var e=$.trim($(".hero__media").data("media-type"));"video"==e?($(".hero__media__slider").remove(),$(".hero__media__video").show(),u()):($(".hero__media__video").remove(),$(".hero__media__slider").show(),c())}
 // Lazyload
-$(".lazy").Lazy({scrollDirection:"vertical",threshold:0}),e(),t(),i(),s(),r(),o(),
+$(".lazy").Lazy({scrollDirection:"vertical",threshold:0}),e(),i(),t(),r(),s(),o(),
 // svg4everybody
 svg4everybody(),
 // SmartMenus
@@ -44,8 +86,6 @@ $(".header__mobile-menu-btn").click(function(e){e.preventDefault(),$(".mobile-me
 // Quicksearch Single Select
 $(".qs-single-select").selectize({create:!1,hideSelected:!0,allowEmptyOption:!0,placeholder:$(this).data("placeholder")}),
 // Quicksearch Multiple Select
-$("#qs_form .multi-select.placeholder").remove(),$("#qs_form .multi-select").show(),$("#qs_form .multi-select").multipleSelect({minimumCountSelected:2,placeholder:$(this).data("placeholder")}),
-// Scroll Navigation Menu
-$(window).scroll(function(){var e=$(this).scrollTop(),t=$(".start-scroll").position();e>$("header").outerHeight()?$(".header__meta").addClass("fixed"):$(".header__meta").removeClass("fixed")}),l(),$(window).resize(function(){a()}),a(),n(),
+$("#qs_form .multi-select.placeholder").remove(),$("#qs_form .multi-select").show(),$("#qs_form .multi-select").multipleSelect({minimumCountSelected:2,placeholder:$(this).data("placeholder")}),l(),$(window).resize(function(){a()}),a(),n(),
 // Enable tooltips
 $('[data-toggle="tooltip"]').tooltip({delay:{show:500,hide:500}}),d(),p()});
